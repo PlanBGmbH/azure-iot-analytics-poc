634635d26f7f40755fb5f05604a1081d75d86629
Markus Meyer <Markus.Meyer@plan-b-gmbh.com>
2021-03-04 07:53:05 +0100

    IoTDevice

--- setIotHubConnectionString.sh
+++ setIotHubConnectionString.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+device_name="MyDotnetDevice"
+
 echo "Getting IoT Values values"
 
 echo
@@ -6,18 +8,18 @@ iot_hub_name=$(az iot hub list --query "[].name" --output tsv)
 iot_hub_name=$(echo "$iot_hub_name" | tr -d '\r')
 echo "iot_hub_name:" $iot_hub_name
 
-iot_hub_device=$(az iot hub device-identity list --hub-name $iot_hub_name --query "[?deviceId == 'MyDotnetDevice'].deviceId")
+iot_hub_device=$(az iot hub device-identity list --hub-name $iot_hub_name --query "[?deviceId == '$device_name'].deviceId")
 iot_hub_device=$(echo "$iot_hub_device" | tr -d '\r')
 echo "iot_hub_device:" $iot_hub_device
 
-if [[ $iot_hub_device == *"MyDotnetDevice"* ]]; then
+if [[ $iot_hub_device == *"$device_name"* ]]; then
     echo "Device exists"
 else
     echo "Create Device"
-    az iot hub device-identity create --hub-name $iot_hub_name --device-id MyDotnetDevice
+    az iot hub device-identity create --hub-name $iot_hub_name --device-id $device_name
 fi
 
-iot_hub_connection_string=$(az iot hub device-identity connection-string show --hub-name $iot_hub_name --device-id MyDotnetDevice --query "connectionString")
+iot_hub_connection_string=$(az iot hub device-identity connection-string show --hub-name $iot_hub_name --device-id $device_name --query "connectionString")
 iot_hub_connection_string=$(echo "$iot_hub_connection_string" | tr -d '\r')
 echo "iot_hub_connection_string:" $iot_hub_connection_string
 
